81af0f99d0ccbc4fb6cfae93c9d0f62158dccbbf
ROHIT <100354219+rohit-krish@users.noreply.github.com>
2022-04-20 11:58:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 /* https://www.a1k0n.net/2006/09/15/obfuscated-c-donut.html */
-/* https://www.a1k0n.net/2006/09/15/obfuscated-c-donut.html */
 /* https://www.a1k0n.net/2011/07/20/donut-math.html */
 
 #include <stdio.h>
@@ -12,12 +11,12 @@ const float thetaStep = 0.01;
 const float TWO_PI = 6.28;
 const char *shade_chars = ".,-~:;=!*#$@";
 const int width = 80;
-const int height = 22;
+const int height = 30;
 const size_t buffer_size = height * width;
 
 void display_buffer(size_t height,size_t width,char *buffer){
     printf("\x1b[H");
-    for (int k = 0; height*width >= k; k++)
+    for (int k = 0; k <= height * width; k++)
         putchar(k % width ? buffer[k] : 10); // 10 => ASCII(\n)
 }
 
@@ -37,12 +36,12 @@ void iterate_buffer(int height,int width,float A,float B,char *buffer,size_t buf
             float D = 1 / (sin_theta * h * sin_A + sin_phi * cos_A + 5);
             float cos_theta = cos(theta);
             float cos_B = cos(B);
-            float n = sin(B);
+            float sin_b = sin(B);
             float t = sin_theta * h * cos_A - sin_phi * sin_A;
-            int x = (width / 2) + 30 * D * (cos_theta * h * cos_B - t * n);
-            int y = (height / 2 - 1) + 15 * D * (cos_theta * h * n + t * cos_B);
+            int x = (width / 2) + 30 * D * (cos_theta * h * cos_B - t * sin_b);
+            int y = (height / 2 - 1) + 15 * D * (cos_theta * h * sin_b + t * cos_B);
             int o = x + width * y;
-            int N = 8 * ((sin_phi * sin_A - sin_theta * cos_phi * cos_A) * cos_B - sin_theta * cos_phi * sin_A - sin_phi * cos_A - cos_theta * cos_phi * n);
+            int N = 8 * ((sin_phi * sin_A - sin_theta * cos_phi * cos_A) * cos_B - sin_theta * cos_phi * sin_A - sin_phi * cos_A - cos_theta * cos_phi * sin_b);
             if (height > y && y > 0 && x > 0 && width > x && D > z[o]) {
                 z[o] = D;
                 buffer[o] = shade_chars[N > 0 ? N : 0];
@@ -61,8 +60,7 @@ int main() {
     while (1){
         iterate_buffer(height,width,A,B,buffer,buffer_size);
         display_buffer(height,width,buffer);
-        A += 0.04;
-        B += 0.02;
+        A += 0.04; // A => rotation about the x axis
+        B += 0.01; // B => rotation about the z axis
     }
 }
-
